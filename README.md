84d3a173252d5483d0ffd0e23959443fa128c5b4
Fritz Schrogl <fsdev+brz@schrogl.at>
2014-06-05 11:27:53 +0200

    Import works now

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 s9y-bloggerXml-importer
 =======================
 
-Rudimentary PHP script. Parses Blogger's XML export and stores posts into Serendipity
+Rudimentary PHP script. Parses Blogger's XML export and stores posts into Serendipity.
+
+Used it to import posts of my private tiny blog from Blogger to my own Serendipity
+installation, therefore author is hardcoded and categories ignored.
--- src/main/bloggerXmlImporter.php
+++ src/main/bloggerXmlImporter.php
@@ -33,9 +33,9 @@ function importEntries($start, $end, $xml) {
 	
 	// Database
 	$dbh = new PDO(DB_CONNECTION, DB_USER, DB_PASS);
-	$stmt = $dbh->prepare("INSERT INTO " . DB_TABLENAME . " 
-				(title, timestamp, body, comments, trackbacks, extended, exflag, author, authorid, isdraft, allow_comments, last_modified, moderate_comments)
-			VALUES (:title, :timestamp, :body, :comments, :trackbacks, :extended, :exflag, :author, :authorid, :isdraft, :allow_comments, :last_modified, :moderate_comments)");
+	$stmt = $dbh->prepare("INSERT INTO " . DB_TABLENAME 
+				. "(title, timestamp, body, comments, trackbacks, extended, exflag, author, authorid, isdraft, allow_comments, last_modified, moderate_comments)"
+				. "VALUES (:title, :timestamp, :body, :comments, :trackbacks, :extended, :exflag, :author, :authorid, :isdraft, :allow_comments, :last_modified, :moderate_comments)");
 	
 	// Insert entries
 	$dbh->beginTransaction();
@@ -46,17 +46,22 @@ function importEntries($start, $end, $xml) {
 					  'last_modified'	 => time(),
 					  'body'			 => "<p>\n<i></i>\n</p>",
 					  'comments'		 => (int) 0,
-					  'moderate_comments'=> false,
+					  'moderate_comments'=> true,
 					  'trackbacks'		 => (int) 0,
 					  'extended'		 => (string) $xml->entry[$i]->content,
 				      'exflag'			 => (int) 1,
 					  'isdraft' 		 => true,
-					  'allow_comments'	 => false,
+					  'allow_comments'	 => 'false',
 					  'author'			 => 'Fritz Schrogl',
 					  'authorid'		 => (int) 1);
 		
 		// Convert timestamp to Unix-Time
-		$datetime = date_create_from_format("Y-m-d?H:i:s?????T", $post['timestamp']);
+		$post['timestamp'][10] = ' ';
+		$post['timestamp'][19] = ' ';
+		$post['timestamp'][20] = ' ';
+		$post['timestamp'][21] = ' ';
+		$post['timestamp'][22] = ' ';
+		$datetime = date_create_from_format("Y-m-d H:i:s T", $post['timestamp']);
 		$post['timestamp'] = $datetime->getTimestamp();
 		
 		$stmt->execute($post);
